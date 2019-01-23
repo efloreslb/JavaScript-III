f80c87773f2ed489708076df69abbad5954c0e7d
Edgar F <eflores.ef@gmail.com>
2019-01-22 23:29:25 -0800

    Finished this.js

--- assignments/this.js
+++ assignments/this.js
@@ -7,7 +7,8 @@
 
 * 3. New Binding - When creating a constructor, .this is used as a placeholder for the object to be created. The keyword "New" is used when making a new object and the name of the object will replace the .this keyword inside the constructor.
 
-* 4. Explicit Binding - 
+* 4. Explicit Binding - The call, apply, and bind methods are used to link a function that uses .this with an object to take place of the .this keyword. This will only happen in that instance.
+
 *
 * write out a code example of each explanation above
 */
@@ -16,14 +17,53 @@
 
 // code example for Window Binding
 
+let greet = function() {
+    console.log(this.name);
+};
+
+greet();
+
 // Principle 2
 
 // code example for Implicit Binding
+
+let teacher = {
+    name: 'Edgar',
+    age: 28,
+    speak : function() {
+        console.log(this.name);
+    }
+};
+
+teacher.speak();
 
 // Principle 3
 
 // code example for New Binding
 
+let Student = function(name, gradYear, gpa) {
+    this.name = name;
+    this.gradYear = gradYear;
+    this.gpa = gpa;
+};
+
+let sebastian = new Student('Sebastian', '2008', 4.0);
+
+console.log(sebastian);
+
 // Principle 4
 
 // code example for Explicit Binding
+
+let speakUp = function(stren01, stren02, stren03) {
+    console.log("Hello, I am " + this.name + " " + "and I am " + stren01 + ", " + stren02 + ", and " + stren03);
+};
+
+let pupil = {
+    name: 'Santiago',
+    age: 22
+};
+
+let strengths = ['smart', 'funny', 'supersmart'];
+
+speakUp.apply(pupil, strengths);
