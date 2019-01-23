7e89b5c077e03aec0712192c2d6ec623ee00990f
Edgar F <eflores.ef@gmail.com>
2019-01-22 20:19:07 -0800

    Initial commit. Working on this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,13 @@
-/* The for principles of "this";
+/* The four principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - By default, .this will refer to the console/window if it is in the global scope and if it is not referring to any predefined object or using any of the other binding principles.
+
+* 2.  Implicit Binding - Using .this immediately after an object that has been created, or a placeholder for an object. .this will refer to the object to the left of the dot
+
+* 3. New Binding - When creating a constructor, .this is used as a placeholder for the object to be created. The keyword "New" is used when making a new object and the name of the object will replace the .this keyword inside the constructor.
+
+* 4. Explicit Binding - 
 *
 * write out a code example of each explanation above
 */
